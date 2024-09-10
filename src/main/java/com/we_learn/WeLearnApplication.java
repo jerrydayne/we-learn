32e5bdd82cc4dc68833d926c9e70555d5581004a
jerrydayne <jerrydaexplorer@gmail.com>
2024-09-10 19:19:42 +0100

    initial setp and database configuration

---
+++ src/main/java/com/we_learn/WeLearnApplication.java
@@ -0,0 +1,13 @@
+package com.we_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeLearnApplication.class, args);
+	}
+
+}
